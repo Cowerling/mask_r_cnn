1179c99d23cff3550c073cfa445b1d08807ae8df
cowerling <cowerling@gmail.com>
2019-11-11 08:52:39 +0800

    fix some bugs

--- application/house_mapping/gewei_train.py
+++ application/house_mapping/gewei_train.py
@@ -1,42 +1,47 @@
+import sys
 import os
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "../..")))
 
 from application.base.training import train
 from application.base.training import TrainingMode
 from application.base.training import augment
 
-work_space = '/home/cowerling/house_mapping'
+work_space = '/home/cowerling/DeepLearning/house/gewei'
 
 train_sources = [{
     'rs_image_path': os.path.join(work_space, 'image/gewei.tif'),
-    'database': 'house_mapping',
+    'database': 'house',
     'user': 'postgres',
-    'password': 'Admin948',
-    'host': '10.6.72.213',
+    'password': 'Cowerling',
+    'host': 'localhost',
     'port': 5432,
-    'mask_table': 'gewei_mask',
-    'bound_table': 'gewei_bound',
+    'mask_table': 'sample_mask_gewei',
+    'bound_table': 'sample_bound_gewei',
     'condition': 'id <= 2000'
 }]
 
 validation_sources = [{
     'rs_image_path': os.path.join(work_space, 'image/gewei.tif'),
-    'database': 'house_mapping',
+    'database': 'house',
     'user': 'postgres',
-    'password': 'Admin948',
-    'host': '10.6.72.213',
+    'password': 'Cowerling',
+    'host': 'localhost',
     'port': 5432,
-    'mask_table': 'gewei_mask',
-    'bound_table': 'gewei_bound',
+    'mask_table': 'sample_mask_gewei',
+    'bound_table': 'sample_bound_gewei',
     'condition': 'id > 2000'
 }]
 
-train(source='gewei_house', class_names=['house'],
+train(source='house_gewei', class_names=['house'],
       train_sources=train_sources,
       validation_sources=validation_sources,
       weight_path=os.path.join(work_space, 'weight/pretrained_weights.h5'),
       log_dir=os.path.join(work_space, 'log'),
       image_dimension=(512, 512),
-      epochs=100,
+      epochs=1000,
+      steps_per_epoch=100, validation_steps=50,
       training_mode=TrainingMode.HEADS,
+      gpu_count=2,
       augmentation=augment(scale=(0.5, 1.5), rotate=(-45, 45), fliplr=0.5),
-      name='gewei')
+      name='house_gewei')
--- application/house_mapping/xiaozede_detection.py
+++ application/house_mapping/xiaozede_detection.py
@@ -1,19 +1,22 @@
+import sys
 import os
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "../..")))
 
 from application.base.detection import detect
 
-work_space = '/home/cowerling/house_mapping'
+work_space = '/home/cowerling/DeepLearning/house_xiaozede'
 
 detect(rs_image_path=os.path.join(work_space, 'image/xiaozede_sub.tif'),
        class_names=['BG', 'house'],
-       weight_path=os.path.join(work_space, 'weight/mask_rcnn_gewei_0072.h5'),
+       weight_path=os.path.join(work_space, 'weight/mask_rcnn_house_gewei_0100.h5'),
        log_dir=os.path.join(work_space, 'log'),
-       database='house_mapping',
+       database='house',
        user='postgres',
-       password='Admin948',
-       host='10.6.72.213',
+       password='Cowerling',
+       host='localhost',
        port=5432,
-       mask_table='xiaozede_house',
-       block_table='xiaodeze_bound',
+       mask_table='detect_house_xiaozede',
+       block_table='detect_bound_xiaozeze',
        bound_size=(1024, 1024),
        bound_buffer=(5, 5))
