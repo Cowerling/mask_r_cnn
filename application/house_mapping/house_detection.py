5a17927a177443a65570105b7b82aba2ca348e8d
cowerling <cowerling@gmail.com>
2020-04-27 21:53:41 +0800

    docker socket

--- application/house_mapping/house_detection.py
+++ application/house_mapping/house_detection.py
@@ -7,10 +7,10 @@ if __name__ == '__main__':
 
     from application.base.remote_detection import remote_detect
 
-    work_space = '/home/cowerling/DeepLearning/house/xiaozede'
-    data_space = '/media/cowerling/share/data'
+    work_space = '/opt/work'
+    data_space = '/opt/data'
 
-    remote_detect(host='10.6.72.250', port=9090,
+    remote_detect(host='0.0.0.0', port=9090,
                   class_names=['BG', 'house'],
                   weight_path=os.path.join(work_space, 'weight/mask_rcnn_house_gewei_0100.h5'),
                   log_dir=os.path.join(work_space, 'log'),
